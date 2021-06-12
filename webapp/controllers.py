f6b2842e1b04acc0113292d9540fc525da05e52a
Pritindra <pritindra.d@gmail.com>
2021-06-12 09:02:12 +0530

    controllers updated

--- webapp/controllers.py
+++ webapp/controllers.py
@@ -1,8 +1,11 @@
 from flask import request, render_template, jsonify, flash
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
 from .models import *
-
+import sys
+from Bio.SeqUtils import ProtParamData
+from Bio.SeqUtils import IsoelectricPoint
+from Bio.Seq import Seq
+from Bio.Data import IUPACData
+from Bio.SeqUtils import molecular_weight
 
 def flash_errors(form):
     """Generate flashes for errors"""
@@ -14,20 +17,61 @@ def flash_errors(form):
             ), 'error')
 
 
-# e.g. controllers
-def register():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        abort(400) # missing arguments
-    if User.query.filter_by(username = username).first() is not None:
-        abort(400) # existing user
-    usr = User(username = username)
-    usr.hash_password(password)
-    db.session.add(usr)
-    db.session.commit()
+# e.g. only peptools
+
+class PepTool:
+    def __init__(self, prot_sequence, monoisotopic=False):
+        if prot_sequence.islower():
+            self.sequence = Seq(prot_sequence.upper())
+        else:
+            self.sequence = Seq(prot_sequence)
+        self.amino_acids_content = None
+        self.amino_percent = None
+        self.length = len(self.sequence)
+        self.monoisotopic = monoisotopic
 
 
-def login(username,password):
-    if username in user and check_password_hash(user.get(username), password):
-        return username
+
+    def molecular_weight(self):
+        return molecular_weight(
+            self.sequence, seq_type="protein", monoisotopic=self.monoisotopic
+        )
+
+    def amino_count(self):
+        if self.amino_acids_content is None:
+            prot_dic = {k: 0 for k in IUPACData.protein_letters}
+            for aa in prot_dic:
+                prot_dic[aa] = self.sequence.count(aa)
+
+            self.amino_acids_content = prot_dic
+
+        return self.amino_acids_content
+
+    def get_amino_percent(self):
+        if self.amino_percent is None:
+            aa_counts = self.amino_count()
+
+            percentages = {}
+            for aa in aa_counts:
+                percentages[aa] = aa_counts[aa] / float(self.length)
+
+            self.amino_percent = percentages
+
+        return self.amino_percent
+
+    def isoelectric_point(self):
+        aa_content = self.amino_count()
+
+        ie_point = IsoelectricPoint.IsoelectricPoint(self.sequence, aa_content)
+        return ie_point.pi()
+
+    def charge_at_pH(self, pH):
+        aa_content = self.amino_count()
+        charge = IsoelectricPoint.IsoelectricPoint(self.sequence, aa_content)
+        return charge.charge_at_pH(pH)
+    
+    def molar_extinction_coefficient(self):
+        num_aa = self.amino_count()
+        mec_reduced = num_aa["W"] * 5500 + num_aa["Y"] * 1490
+        mec_cystines = mec_reduced + (num_aa["C"] // 2) * 125
+        return (mec_reduced, mec_cystines)
--- webapp/routes.py
+++ webapp/routes.py
@@ -3,13 +3,12 @@ from . import controllers
 
 
 # e.g blueprint and routes
-auth_blueprint = Blueprint("auth",__name__, template_folder="templates", static_folder='static')
+auth_blueprint = Blueprint("auth",__name__, template_folder="ui/templates", static_folder='ui/static')
 # auth_blueprint.add_url_rule("register", "register", controllers.register)
 # auth_blueprint.add_url_rule("login", "login", controllers.login)
 
 @auth_blueprint.route('/register')
 def register():
-
     username = request.json.get('username')
     password = request.json.get('password')
     if username is None or password is None:
@@ -20,3 +19,8 @@ def register():
     usr.hash_password(password)
     db.session.add(usr)
     db.session.commit()
+
+@auth_blueprint.route('/login')
+def login(username,password):
+    if username in user and check_password_hash(user.get(username), password):
+        return username
